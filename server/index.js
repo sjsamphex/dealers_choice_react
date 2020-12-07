b4d7fc6cd5ee64a2b3068a7afebf33426b29468d
sjsamphex <sjsamphex@gmail.com>
2020-12-07 12:37:20 -0800

    working style.css

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'src')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 // Routes that will be accessed via AJAX should be prepended with
 // /api so they are isolated from our GET /* wildcard.
