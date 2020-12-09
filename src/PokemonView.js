194cc38f2cf435d918b5504a317fda30e2c83827
sjsamphex <sjsamphex@gmail.com>
2020-12-09 10:46:29 -0800

    implement more pokemon.json data into view

--- src/PokemonView.js
+++ src/PokemonView.js
@@ -4,11 +4,17 @@ function PokemonView(props) {
   const { selectedPokemon, sendToTrainer, trainerData } = props;
   let imgsrc = `/thumbnails/001.png`;
   let pokemonID = `#001: Bulbasaur`;
+  let pokemonData;
   if (selectedPokemon) {
     imgsrc = `/thumbnails/${String(selectedPokemon.id).padStart(3, 0)}.png`;
     pokemonID = `#${String(selectedPokemon.id).padStart(3, 0)}: ${
       selectedPokemon.name
     }`;
+
+    const avoidKeys = ['id', 'name', 'createdAt', 'trainerId', 'updatedAt'];
+    pokemonData = Object.keys(selectedPokemon).filter((info) => {
+      return !avoidKeys.includes(info);
+    });
   }
 
   return (
@@ -17,6 +23,13 @@ function PokemonView(props) {
       <br />
       <img src={imgsrc} title={pokemonID} />
       <hr />
+      <ul>
+        {pokemonData.map((info) => (
+          <li key={info}>
+            {info}: {selectedPokemon[info]}
+          </li>
+        ))}
+      </ul>
       <button onClick={() => sendToTrainer(selectedPokemon.id)}>
         Add {selectedPokemon.name} to {trainerData.name}'s party
       </button>
